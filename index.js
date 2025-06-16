91a71677ced3a198df493efc5e2d618549355391
Franklin Njoroge <njorogefranklin2@gmail.com>
2025-06-16 22:05:13 +0300

    Update index.js
    another

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express();
 app.use(express.json());
 const morgan = require('morgan')
-app.use(morgan('tiny')) 
 morgan.token('post-data', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
